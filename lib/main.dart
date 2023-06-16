2f1d8a312dca5f94e44312909291fc068435b795
alirezayoom <alirezayoom@gmail.com>
2023-06-16 10:49:26 +0430

    add random number to both dices

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,8 @@ class DicePage extends StatefulWidget {
 }
 
 class _DicePageState extends State<DicePage> {
-  int leftDiceNumber = 5;
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -43,6 +44,7 @@ class _DicePageState extends State<DicePage> {
             onPressed: () {
               setState(() {
                 leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
               });
             },
             child: Image.asset("images/dice$leftDiceNumber.png"),
@@ -51,9 +53,12 @@ class _DicePageState extends State<DicePage> {
         Expanded(
           child: TextButton(
             onPressed: () {
-              print('right');
+              setState(() {
+                leftDiceNumber = Random().nextInt(6) + 1;
+                rightDiceNumber = Random().nextInt(6) + 1;
+              });
             },
-            child: Image.asset("images/dice1.png"),
+            child: Image.asset("images/dice$rightDiceNumber.png"),
           ),
         ),
       ],
